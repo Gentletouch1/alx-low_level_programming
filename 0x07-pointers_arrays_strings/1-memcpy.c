3d985f35a1f40979d8f479c5baecebccf09d4d03
Gentletouch1 <adeyemishobukola@gmail.com>
2023-03-08 09:11:21 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-		unsigned int i;
+		int i = 0;
+		int j = n;
 
-			for (i = 0; i < n; i++)
+			for (; i > j; i++)
+			{
 				dest[i] = src[i];
+				n--;
+			}
 
 				return (dest);
 }
